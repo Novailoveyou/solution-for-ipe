a4f91c686e6f4ffb143718d17955c7b239886411
Nova i love you <novailoveyoux@gmail.com>
2020-08-21 13:06:34 +0300

    temporary commented utm params script

--- dist/js/main.js
+++ dist/js/main.js
@@ -14,44 +14,44 @@ fetch('https://api.ipify.org/?format=json')
 
 
 // UTM params
-let utmQuery = decodeURIComponent(window.location.search.substring(1)),
-  utmVariables = utmQuery.split('&'),
-  ParameterName,
-  i;
+// let utmQuery = decodeURIComponent(window.location.search.substring(1)),
+//   utmVariables = utmQuery.split('&'),
+//   ParameterName,
+//   i;
 
-const getUTMValue = (inputParameter) => {
-  for (i = 0; i < utmVariables.length; i++) {
-    ParameterName = utmVariables[i].split('=');
-    if (ParameterName[0] === inputParameter) {
-      return ParameterName[1] === null ? null : ParameterName[1];
-    }
-  }
-};
+// const getUTMValue = (inputParameter) => {
+//   for (i = 0; i < utmVariables.length; i++) {
+//     ParameterName = utmVariables[i].split('=');
+//     if (ParameterName[0] === inputParameter) {
+//       return ParameterName[1] === null ? null : ParameterName[1];
+//     }
+//   }
+// };
 
-const valueExists = (value) => {
-  return value != null && value != '' && value != undefined;
-};
+// const valueExists = (value) => {
+//   return value != null && value != '' && value != undefined;
+// };
 
-const utmParams = [
-  'utm_source',
-  'utm_medium',
-  'utm_campaign',
-  'utm_content',
-  'utm_term',
-];
+// const utmParams = [
+//   'utm_source',
+//   'utm_medium',
+//   'utm_campaign',
+//   'utm_content',
+//   'utm_term',
+// ];
 
-utmParams.forEach((param) => {
-  var pValue = getUTMValue(param);
+// utmParams.forEach((param) => {
+//   var pValue = getUTMValue(param);
 
-  if (valueExists(pValue)) {
-    Cookies.set(param, pValue, {
-      domain: cookieDomain,
-      expires: 90,
-    });
-  }
-  let cValue = Cookies.get(param);
-  let input = document.getElementById(param);
-  if (input && valueExists(cValue)) {
-    input.value = cValue;
-  }
-});
+//   if (valueExists(pValue)) {
+//     Cookies.set(param, pValue, {
+//       domain: cookieDomain,
+//       expires: 90,
+//     });
+//   }
+//   let cValue = Cookies.get(param);
+//   let input = document.getElementById(param);
+//   if (input && valueExists(cValue)) {
+//     input.value = cValue;
+//   }
+// });
